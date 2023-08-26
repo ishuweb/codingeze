925f8b8125ab9eb0a73cc9cbe60aa94fe2a33b78
Ishu Gupta <ishugupta7788@gmail.com>
2023-08-26 13:38:22 +0530

    Homepage Complete. Bottom Section Pending

--- script/homepage.js
+++ script/homepage.js
@@ -26,3 +26,50 @@ function offloader() {
         $(".rotor_img").css("transform","scale(100%)");
     },100)
 }
+
+
+// ===================================================
+// ========= HEADER ANIMATION LEFT SCROLLER ==========
+// ===================================================
+
+function header_scroler_anim() {
+    setInterval(()=>{
+        var mt_height = $(".headbox_scrol_wraper").css("margin-top");
+        if(mt_height == "0px") {
+            $(".headbox_scrol_wraper").css("margin-top","-400px");
+        }
+        else if(mt_height == "-400px") {
+            $(".headbox_scrol_wraper").css("margin-top","-800px");
+        }
+        else if(mt_height == "-800px") {
+            $(".headbox_scrol_wraper").css("margin-top","-1200px");
+        }
+        else if(mt_height == "-1200px") {
+            $(".headbox_scrol_wraper").css("margin-top","-1600px");
+        }
+        else if(mt_height == "-1600px") {
+            $(".headbox_scrol_wraper").css("margin-top","-2000px");
+        }
+        else if(mt_height == "-2000px") {
+            $(".headbox_scrol_wraper").css("margin-top","-2400px");
+        }
+        else if(mt_height == "-2400px") {
+            $(".headbox_scrol_wraper").css("margin-top","-2800px");
+        }
+        else if(mt_height == "-2800px") {
+            $(".headbox_scrol_wraper").css("margin-top","-3200px");
+        }
+        else if(mt_height == "-3200px") {
+            $(".headbox_scrol_wraper").css("margin-top","-3600px");
+        }
+        else if(mt_height == "-3600px") {
+            $(".headbox_scrol_wraper").css("margin-top","-4000px");
+        }
+        else if(mt_height == "-4000px") {
+            $(".headbox_scrol_wraper").css("margin-top","0px");
+        }
+    },4000)
+}
+setTimeout(()=>{
+    header_scroler_anim();
+},2000)
