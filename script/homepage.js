1873a4b66aa490d03f32b9c983575a3e8f5845b3
Ishu Gupta <ishugupta7788@gmail.com>
2023-09-01 00:47:35 +0530

    Hompage Desktop, Semi-desktop and Mobile w/o anime Completed

--- script/homepage.js
+++ script/homepage.js
@@ -10,6 +10,7 @@ function scrolanimate(identifier, style_action, style_val, scroller) {
         if(document.documentElement.scrollTop >= scroller) {
             $(identifier).css(style_action, style_val);
         }
+        console.log(document.documentElement.scrollTop);
     })
 }
 
@@ -25,6 +26,29 @@ function offloader() {
     setTimeout(()=>{
         $(".rotor_img").css("transform","scale(100%)");
     },100)
+
+
+    // =====================================================
+    // ===== ANNIMATION DESKTOP AND SEMI DESKTOP ONLY ======
+    // =====================================================
+
+    if(screen.width >= 900) {
+       
+        // ANIMATION ON SCROLL FUNCTION RUN AFTER PAGE LOADED 
+        // SECTION 3
+        scrolanimate("#sec_3_txt_1_contr", "padding-top", "50px", "4250");
+        scrolanimate("#sec_3_txt_2_contr", "margin-top", "80px", "4320");
+        scrolanimate("#sec_3_txt_3_contr", "margin-top", "40px", "4420");
+        scrolanimate("#sec_3_img_contr", "transform", "scale(100%)", "4420");
+
+        // SECTION 4
+        scrolanimate("#sec_4_txt_1_contr", "padding-top", "120px", "4820");
+        scrolanimate(".sec_4_smbox_eachbox", "transform", "scale(100%)", "4820");
+    }
+    // ANIMATION FOR MOBILE ONLY 
+    else if(screen.width <= 600) {
+       
+    }
 }
 
 
